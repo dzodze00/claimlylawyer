e406ddf12961647f3ca4fc8ee258abb1d620f21c
dzodze00 <pakpablie@gmail.com>
2025-03-19 13:50:54 -0700

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    optimizeCss: true,
+    optimizePackageImports: ['lucide-react'],
+  },
+}
+
+export default nextConfig
+
