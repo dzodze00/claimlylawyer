66808bb5ca580cde4f92a5a0e635514820115eeb
dzodze00 <pakpablie@gmail.com>
2025-03-19 13:47:28 -0700

    Create use-click-outside.ts

---
+++ hooks/use-click-outside.ts
@@ -0,0 +1,25 @@
+"use client"
+
+import type React from "react"
+
+import { useEffect, useRef } from "react"
+
+export function useClickOutside<T extends HTMLElement>(callback: () => void): React.RefObject<T> {
+  const ref = useRef<T>(null)
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback()
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside)
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [callback])
+
+  return ref
+}
+
